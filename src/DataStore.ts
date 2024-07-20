b1165a04e44fc090632b29a3cf5fd520c3d7c3b6
TYPESAFE-RUSTY <155160359+TYPESAFE-RUSTY@users.noreply.github.com>
2024-07-20 11:01:11 +0530

    Added a Card

--- src/DataStore.ts
+++ src/DataStore.ts
@@ -1,4 +1,36 @@
 import { Card } from "./types";
 export let data: Card[] = [
 
+    {
+        "name": "SAHIL SINGH",
+        "description": "Hi I am Sahil. Full Stack Developer with Interest in System Programming and System Design.",
+        "resources": [
+            {
+                "name": "The Book",
+                "url": "https://doc.rust-lang.org/book/"
+            },
+            {
+                "name": "3D on web",
+                "url": "https://threejs.org/"
+            },
+            {
+                "name": "Motivation",
+                "url": "https://www.youtube.com/watch?v=xvFZjo5PgG0"
+            }
+        ],
+        "socials": [
+            {
+                "name": "github",
+                "url": "https://github.com/TYPESAFE-RUSTY"
+            },
+            {
+                "name": "linkedIn",
+                "url": "https://www.linkedin.com/in/sahilsingh03122003/"
+            },
+            {
+                "name": "instagram",
+                "url": "https://www.instagram.com/printf_sahil_/"
+            }
+        ]
+    }
 ]
