e004684aacb1b6f3efa04613afd02761fd63f325
gaurav-foroll <gm890205@gmail.com>
2024-07-23 20:30:18 +0530

    Feat (Type,Component) :
    - Updated types.tsx to add icon field
    - Updated card component handeled edge cases

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,21 @@
 "use client";
 import React from "react";
 import { Card as C, url } from '@/types';
-import { Tilt } from 'react-tilt'
+import { Tilt } from 'react-tilt';
 
 import { FaGithub, FaLinkedin, FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa6";
-
+import { SiGodotengine } from "react-icons/si";
 
 const MAX_DESCRIPTION_LENGTH = 100;
 const MAX_SOCIALS = 5;
+const MAX_RESOURCE = 3;
+const MAX_NAME_LENGTH = 27;
 
 function getIcon(data: url, key: number) {
     let { name, url } = data;
     //(note for future me) remember to compare url to name 
     switch (name.toLowerCase().trim()) {
-        case "github":
+        case "github" || "git hub" || "git-hub":
             return <a key={key} href={url} target="_blank" rel="noopener noreferrer"><FaGithub /></a>
         case "linkedin" || "linked in":
             return <a key={key} href={url} target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
@@ -59,13 +61,14 @@ export default function Card({ className, data }: { className: string, data: C }
             <h3 style={{ fontSize: "medium", marginBlock: "1rem 0.5rem" }}>Resources I&apos;d love to share:</h3>
             <ul>
                 {
-                    data.resources.map((resource, index) => (<li key={index} style={{ marginBlockEnd: "0.25rem" }}>
+                    data.resources.map((resource, index) => (index <=MAX_RESOURCE && <li key={index} style={{ marginBlockEnd: "0.25rem" }}>
                         <a
                             href={resource.url}
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            {resource.name}
+                            {resource.name.length <= MAX_NAME_LENGTH ? resource.name : `${resource.name.slice(0,MAX_NAME_LENGTH )}...`}
+                            {resource.icon && <SiGodotengine fill="#478cbf"/>}
                         </a>
                     </li>))
                 }
